74791f2fbaa2d942a121a767c22bb1d42ef28664
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-14 22:22:06 +0200

    configure gateway for service discovery

--- src/main/java/com/gabriel/msscbrewerygateway/config/LoadBalancedRoutConfig.java
+++ src/main/java/com/gabriel/msscbrewerygateway/config/LoadBalancedRoutConfig.java
@@ -6,27 +6,32 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalhostRoutConfig {
+public class LoadBalancedRoutConfig {
 
     @Bean
-    public RouteLocator localhostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
 
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer", "/api/v1/beer/*","/api/v1/beer/upc/*")
-                        .uri("http://localhost:8080")
+                        .uri("lb://beer-service")
                         .id("beer-service"))
-                        .route(r->r.path("/api/v1/customers/*/orders","/api/v1/customers/*/orders/*","/api/v1/customers/*/orders/*/pickup" )
-                        .uri("http://localhost:8081")
+                .route(r->r.path("/api/v1/customers/*/orders","/api/v1/customers/*/orders/*","/api/v1/customers/*/orders/*/pickup" )
+                        .uri("lb://order-service")
                         .id("order-service"))
-                        .route(r->r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082")
+                .route(r->r.path("/api/v1/beer/*/inventory")
+                        .uri("lb://inventory-service")
                         .id("inventory-service"))
                 .build();
 
     }
 
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
 
 }
